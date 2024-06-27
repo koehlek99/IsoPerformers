43972d7cf0f83ee8cc8c5174d728225b66049ade
Marie Neuhoff <mne@MacBook-Pro-von-Marie.local>
2024-06-27 17:59:01 +0200

    changed minor code errors

--- R/isoform_long_read.R
+++ R/isoform_long_read.R
@@ -1,67 +1,76 @@
+# load necessary libraries
 library(biomaRt)
 library(tidyr)
 library(dplyr)
 library(stringr)
 library(ggplot2)
 library(DRIMSeq)
-
 library(EnsDb.Hsapiens.v86)
-edb <- EnsDb.Hsapiens.v86
-txp <- transcripts(edb, return.type="data.frame")
 library(tibble)
-txp <- txp |> as_tibble()
-library(dplyr)
-tx2gene <- txp |>
-  dplyr::select(transcript=tx_id, gene=gene_id)
 
-rbp <-read.table(gzfile("data/quantification_gencode.counts.txt.gz"),header = T)   
+# get gene symbols from ensembldb
+edb <- EnsDb.Hsapiens.v86
+txp <- transcripts(edb, columns=c("tx_id", "gene_id", "gene_name"), return.type="data.frame")
+txp <- txp |> as_tibble()
+tx2gene <- txp |>
+  dplyr::select(transcript=tx_id, gene=gene_id, symbol = gene_name)
+
+# load count matrix
+rbp <-read.table(gzfile("data/quantification_gencode.counts.txt.gz"),header = T)    # nolint
 rbp$transcript <- unlist(lapply(strsplit(rbp$transcript,split='\\.'),'[[',1))
-samples_exp = names(rbp)[grepl('exp', names(rbp))]
-samples_ctrl = names(rbp)[grepl('ctr', names(rbp))]
-rbp = rbp %>% dplyr::select(transcript, samples_exp, samples_ctrl)
-rbp = rbp[rowSums(rbp[c(samples_exp, samples_ctrl)])!=0,]
-rbp = merge(rbp, tx2gene, by.x = "transcript", by.y = "tx_id", all.x = TRUE)
-rbp = rbp[c("gene_id","transcript", samples_exp, samples_ctrl)]
+
+# define what samples are control and what are knockdowns (exp)
+samples_exp <- names(rbp)[grepl('exp', names(rbp))]
+samples_ctrl <- names(rbp)[grepl('ctr', names(rbp))]
+
+# add experiment information (ctr, exp) to rbp
+rbp <- rbp |> dplyr::select(transcript, samples_exp, samples_ctrl)
+# filter transcripts that have no expression in any sample
+rbp <- rbp[rowSums(rbp[c(samples_exp, samples_ctrl)])!=0,]
+
+# add gene symbol based on transcript
+rbp <- merge(rbp, tx2gene, by.x = "transcript", by.y = "transcript", all.x = TRUE) # nolint
+# reorder columns
+rbp <- rbp[c("gene", "symbol", "transcript", samples_exp, samples_ctrl)]
 
 ##########################################
 ## an alternative approach to the above ##
 ##########################################
-library(readr)
-counts <- read_delim("data/quantification_gencode.counts.txt.gz")
-counts <- counts |>
-  mutate(transcript = sub("\\..*","",transcript)) |>
-  left_join(tx2gene) |>
-  dplyr::select(transcript, gene, contains("exp"), contains("ctr"))
-counts <- counts %>%
-  mutate(rowsum = rowSums(dplyr::select(., contains(c("exp","ctr"))))) |>
-  dplyr::filter(rowsum > 0) |>
-  dplyr::select(-rowsum)
+#library(readr)
+#rbp <- read_delim("data/quantification_gencode.counts.txt.gz")
+#rbp <- rbp |>
+#  mutate(transcript = sub("\\..*","",transcript)) |>
+#  left_join(tx2gene) |>
+#  dplyr::select(transcript, gene, contains("exp"), contains("ctr"))
+#rbp <- rbp |>
+#  mutate(rowsum = rowSums(dplyr::select(., contains(c("exp","ctr"))))) |>
+#  dplyr::filter(rowsum > 0) |>
+#  dplyr::select(-rowsum)
 ###########################################
 
-##number of transcripts without gene id
-rbp %>% subset(is.na(gene_id)) %>% nrow()
-rbp_filt = na.omit(rbp)
+## number of transcripts without gene id
+rbp |> subset(is.na(gene)) |> nrow()
+# filter these out
+rbp_filt <- na.omit(rbp)
 #rbp_filt = rbp_filt %>% subset(hgnc_symbol!="")
+length(unique(rbp_filt$gene))
 
-##Number of unique genes
-length(unique(rbp_filt$gene_id))
-
-##Number of unique transcript
+## Number of unique transcript
 length(unique(rbp_filt$transcript))
 
-NrTranscripts = rbp_filt %>% group_by(gene_id) %>% summarise(NrTranscript = n_distinct(transcript))
+NrTranscripts = rbp_filt |> group_by(gene)|> summarise(NrTranscript = n_distinct(transcript)) # nolint
 ggplot(NrTranscripts) + geom_boxplot(aes(y=NrTranscript))
-NrTranscripts %>% arrange(-NrTranscript)
+NrTranscripts |> arrange(-NrTranscript)
 
 #rbp_filt %>% subset(hgnc_symbol=="MUC20-OT1") %>% select(transcript)
 
-PTBP1_counts = rbp_filt %>% subset(gene_id=="ENSG00000011304")
+PTBP1_counts = rbp_filt |> subset(gene=="ENSG00000011304") 
 PTBP1_counts_long = pivot_longer(PTBP1_counts, cols = c(samples_exp, samples_ctrl))
 PTBP1_counts_long$condition = ifelse(grepl('exp', PTBP1_counts_long$name), 'knockout', 'control')
-png('plots/PTBPC_transcriptExp.png')
-ggplot(PTBP1_counts_long) + geom_boxplot(aes(x=condition, y = value, fill = condition)) + facet_wrap(~transcript)
-dev.off()
-
+PTBP1_expression_plot <- ggplot(PTBP1_counts_long) + geom_boxplot(aes(x=condition, y = value, fill = condition)) + facet_wrap(~transcript)
+PTBP1_expression_plot
+# Save the plot to a file
+ggsave(filename = "./plots/PTBP1_boxplot.pdf", plot = PTBP1_expression_plot, width = 10, height = 8, dpi = 600)
 
 #create colData df
 sample_dat <- data.frame(sample_id = c(samples_ctrl, samples_exp))
@@ -92,43 +101,79 @@ explained_variance <- variance / sum(variance)
 png('plots/explainedVar.png')
 plot(explained_variance)
 dev.off()
-
+plot(explained_variance)
 rotation <- as.data.frame(pca$rotation)
 pcs <- as.data.frame(pca$x)
 pcs$condition <- ifelse(grepl('exp', rownames(pcs)), 'knockout', 'control')
 pcs$sample <- rownames(pcs) 
 
 png('plots/PCA_highVar.png')
-ggplot(pcs, aes(x=PC1, y = PC2, color = condition)) + geom_point() + 
-  geom_text(aes(label = sample),show.legend=FALSE, vjust = -1, hjust = 0.5)
+ggplot(pcs, aes(x=PC1, y = PC2, color = condition)) + geom_point() 
 dev.off()
 
+ggplot(pcs, aes(x=PC1, y = PC2, color = condition)) + geom_point()
 
 ##get features (transcripts) with highest loadings for PC1 and 2
-pc1_idx = rownames(rotation %>% arrange(-PC1) %>% head(10))
-pc2_idx = rownames(rotation %>% arrange(-PC2) %>% head(10))
+pc1_idx <- rownames(rotation |> arrange(-PC1) |> head(10))
+pc2_idx <- rownames(rotation |> arrange(-PC2) |> head(10))
 
 ##extract corresponding gene ids 
-top10_pc1_trans = rbp_filt[pc1_idx,'transcript']
-top10_pc1_gene = rbp_filt[pc1_idx,'gene_id']
+top10_pc1_trans <-  rbp_filt[pc1_idx,'transcript']
+top10_pc1_gene <- rbp_filt[pc1_idx,c('symbol', 'gene')]
 
-top10_pc2_trans = rbp_filt[pc2_idx,'transcript']
-top10_pc2_gene = rbp_filt[pc2_idx,'gene_id']
+top10_pc2_trans <-  rbp_filt[pc2_idx,'transcript']
+top10_pc2_gene <- rbp_filt[pc2_idx,c('symbol', 'gene')]
 
 ##plots all transcripts per gene 
 plotTranscriptExp = function(gene, counts, plots){
-  tmp_count = counts %>% subset(gene_id==gene)
-  tmp_count = pivot_longer(tmp_count, cols = c(samples_exp, samples_ctrl))
+  # Subset the counts data for the specified gene
+  tmp_count <- counts |> dplyr::filter(gene == gene)
+  # Pivot the data to a longer format
+  tmp_count <- pivot_longer(tmp_count, cols = c(samples_exp, samples_ctrl))
   tmp_count$condition = ifelse(grepl('exp', tmp_count$name), 'knockout', 'control')
   
   plots[[gene]] =  ggplot(data.frame(tmp_count)) + 
                   geom_boxplot(aes(x=condition, y = value, fill = condition)) + 
                   facet_wrap(~transcript) + 
-                  ggtitle('Transcript isoforms of ', gene)
+                  ggtitle(paste('Transcript isoforms of ', symbol))
   return(plots)
 }
+gene_input <- top10_pc1_gene[,2]
+test <- rbp_filt |> dplyr::filter(gene == gene_input)
+test <- pivot_longer(test, cols = c(samples_exp, samples_ctrl))
+test$condition = ifelse(grepl('exp', test$name), 'knockout', 'control')
+plots[[gene_input]] =  ggplot(data.frame(test)) + 
+                  geom_boxplot(aes(x=condition, y = value, fill = condition)) + 
+                  facet_wrap(~transcript) 
 
-p1 = lapply(top10_pc1_gene, plotTranscriptExp, counts = rbp_filt, plots = list())
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# Initialize an empty list to store plots
+plots <- list()
+
+# Generate plots for top 10 genes
+for (gene in unique(top10_pc1_gene$gene)) {
+  plots <- plotTranscriptExp(gene, rbp_filt, plots)
+}
+
+
+
+p1 = lapply(top10_pc1_gene, plotTranscriptExp, counts = rbp_filt, plots = list()) # nolint
+p1
 p2 = lapply(top10_pc2_gene, plotTranscriptExp, counts = rbp_filt, plots = list())
 
 pdf(paste0('plots/topTranscripts_PC1.pdf'))
@@ -139,8 +184,6 @@ dev.off()
 pdf(paste0('plots/topTranscripts_PC2.pdf'))
 p2
 dev.off()
-
-
 
 ##DrimSeq filtering
 counts_drim = rbp_filt %>% dplyr::rename(feature_id=transcript)
@@ -177,3 +220,4 @@ dev.off()
 ##DESeq2- To-Do?
 
 
+?lapply
--- README.md
+++ README.md
@@ -4,9 +4,18 @@ Isoform analysis using Nanopore data
 
 publication: https://doi.org/10.1038%2Fs41586-022-05035-y
 
-Roadmap:
 
+useful tools for protein/RNA levels/biological meaning: 
+  https://www.proteinatlas.org/ENSG00000011304-PTBP1/tissue
+
+Roadmap:
+    1) Quality control: is the knockdown really a knockdown?
+    
     DRIMSeq for filtering
     DEXSeq for differential splicing
     IsoformSwitchAnalyzeR for consequence
     plotting?
+
+Todo:
+  PCA for control vs treated --> what drives PCA component 1?
+  
